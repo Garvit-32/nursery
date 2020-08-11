1cc4c0858a2d394bf66d8dcdedeba03a7cf4a586
Garvit-32 <garvitgarg32@gmail.com>
2020-08-11 13:16:44 +0530

    commit

--- nursery/settings.py
+++ nursery/settings.py
@@ -48,6 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Simplified static files serving
+    # https://warehouse.python.org/project/whitenoise
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'nursery.urls'
